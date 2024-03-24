00358e62cc0cd80552bb5c2a798fc9f9fba5ae3a
Muntasir <bvllr5@inf.elte.hu>
2024-03-24 13:56:45 +0100

    Track's One to Many Relationship is created in Project and Track Controller

--- database/migrations/2024_03_24_124155_create_tracks_table.php
+++ database/migrations/2024_03_24_124155_create_tracks_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('tracks', function (Blueprint $table) {
             $table->id();
+
+            $table->string("name", 100);
+            $table->string("filename", 200)->nullable();
+            $table->string("color")->default("#ffffff"); // Default color is white if not specified
+
             $table->timestamps();
         });
     }
