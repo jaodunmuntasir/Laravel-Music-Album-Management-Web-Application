d7e0a8ba8394ada74edac8d02defd2ebafaceb83
Muntasir <bvllr5@inf.elte.hu>
2024-04-01 03:38:05 +0200

    Databaase Path Adjusted

--- config/database.php
+++ config/database.php
@@ -38,7 +38,7 @@ return [
         'sqlite' => [
             'driver' => 'sqlite',
             'url' => env('DATABASE_URL'),
-            'database' => database_path(env('DB_DATABASE', 'database.sqlite')),
+            'database' => database_path(env('DB_DATABASE', 'mytracks.sqlite')),
             'prefix' => '',
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
